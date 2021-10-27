af783a376951a9bab3dedb886cf4df82ef30c834
kare <58642627+kareotoko@users.noreply.github.com>
2021-10-27 10:10:25 +0900

    Update ArmstrongNumber81.cpp
    added typo

--- ArmstrongNumber81.cpp
+++ ArmstrongNumber81.cpp
@@ -4,21 +4,22 @@ using namespace std;
 int main()
 {
 	int n,r,sum=0,m;
-	cout<<"Enter the Number";
+	cout<<"Enter the Number ";
 	cin>>n;
 	m=n;
 	    
 	while(n>0)
 	{
-		r=n%10;
-	    n=n/10;
-		sum=sum+r*r*r;
+	    r=n%10;
+            sum=sum+r*r*r;
+            n/=10;
 	}
+	cout<<"sum is "<<sum<<endl;
 	    
 	if(sum==m)
 	    cout<<"Armstrong Number";
 	else
-		cout<<"Not a Armstrong Number";
+	    cout<<"Not a Armstrong Number";
 		
 	return 0;
 	    
