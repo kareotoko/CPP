a8e1b0498cd5576776873c5a01f0c940c5c625da
kare <58642627+kareotoko@users.noreply.github.com>
2021-10-27 11:19:44 +0900

    Update ArrayLinearSearch92.cpp
    added comment

--- ArrayLinearSearch92.cpp
+++ ArrayLinearSearch92.cpp
@@ -1,24 +1,28 @@
 #include<iostream>
 using namespace std;
 
+// this algo used to search from beg to end one by one
+// time complexity is O(n)
+
 int main()
 {
  	int A[10],n=10;
 	int key;
-	cout<<"Enter Numbers";
+	cout<<"Enter Numbers of ten array: ";
 	    
 	for(int i=0;i<n;i++)
 	{
 		cin>>A[i];
 	}
-	cout<<"Enter Key";
+	
+	cout<<"Enter Key: ";
 	cin>>key;
 	    
 	for(int i=0;i<n;i++)
 	{
 		if(key==A[i])
 		{
-			cout<<"Found at"<<i;
+			cout<<"Found at "<<i;
 			return 0;
 		}
 	}
